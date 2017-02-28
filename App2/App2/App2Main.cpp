2bdb223de5b3964442ee74f2aa984d10de09cda6
Cody Stanko <cstanko@fullsail.edu>
2017-02-28 17:11:43 -0500

    Initial creation of project, base model

---
+++ App2/App2/App2Main.cpp
@@ -0,0 +1,123 @@
+﻿#include "pch.h"
+#include "App2Main.h"
+#include "Common\DirectXHelper.h"
+
+#include <Windows.h>
+
+using namespace App2;
+using namespace Windows::Foundation;
+using namespace Windows::System::Threading;
+using namespace Concurrency;
+
+// Loads and initializes application assets when the application is loaded.
+App2Main::App2Main(const std::shared_ptr<DX::DeviceResources>& deviceResources) :
+	m_deviceResources(deviceResources)
+{
+	// Register to be notified if the Device is lost or recreated
+	m_deviceResources->RegisterDeviceNotify(this);
+
+	// TODO: Replace this with your app's content initialization.
+	m_sceneRenderer = std::unique_ptr<Sample3DSceneRenderer>(new Sample3DSceneRenderer(m_deviceResources));
+
+	m_fpsTextRenderer = std::unique_ptr<SampleFpsTextRenderer>(new SampleFpsTextRenderer(m_deviceResources));
+
+	// TODO: Change the timer settings if you want something other than the default variable timestep mode.
+	// e.g. for 60 FPS fixed timestep update logic, call:
+	/*
+	m_timer.SetFixedTimeStep(true);
+	m_timer.SetTargetElapsedSeconds(1.0 / 60);
+	*/
+}
+
+App2Main::~App2Main()
+{
+	// Deregister device notification
+	m_deviceResources->RegisterDeviceNotify(nullptr);
+}
+
+// Updates application state when the window size changes (e.g. device orientation change)
+void App2Main::CreateWindowSizeDependentResources() 
+{
+	// TODO: Replace this with the size-dependent initialization of your app's content.
+	m_sceneRenderer->CreateWindowSizeDependentResources();
+}
+
+using namespace Windows::UI::Core;
+extern CoreWindow^ gwindow;
+// Updates the application state once per frame.
+void App2Main::Update() 
+{
+	if (Windows::UI::Core::CoreVirtualKeyStates::Down == gwindow->GetAsyncKeyState(Windows::System::VirtualKey::Space))
+	{
+		Windows::UI::Input::PointerPoint^ point = Windows::UI::Input::PointerPoint::GetCurrentPoint(1);
+		float X = point->Position.X;
+		float Y = point->Position.Y;
+	}
+
+	// Update scene objects.
+	m_timer.Tick([&]()
+	{
+		// TODO: Replace this with your app's content update functions.
+		m_sceneRenderer->Update(m_timer);
+		m_fpsTextRenderer->Update(m_timer);
+	});
+}
+
+// Renders the current frame according to the current application state.
+// Returns true if the frame was rendered and is ready to be displayed.
+bool App2Main::Render() 
+{
+	// Don't try to render anything before the first Update.
+	if (m_timer.GetFrameCount() == 0)
+	{
+		return false;
+	}
+
+	auto context = m_deviceResources->GetD3DDeviceContext();
+
+	// Reset the viewport to target the whole screen.
+	auto viewport = m_deviceResources->GetScreenViewport();
+	context->RSSetViewports(1, &viewport);
+
+	// Reset render targets to the screen.
+	ID3D11RenderTargetView *const targets[1] = { m_deviceResources->GetBackBufferRenderTargetView() };
+	context->OMSetRenderTargets(1, targets, m_deviceResources->GetDepthStencilView());
+
+	// Clear the back buffer and depth stencil view.
+	context->ClearRenderTargetView(m_deviceResources->GetBackBufferRenderTargetView(), DirectX::Colors::CornflowerBlue);
+	context->ClearDepthStencilView(m_deviceResources->GetDepthStencilView(), D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
+
+	// Render the scene objects.
+	// TODO: Replace this with your app's content rendering functions.
+	m_sceneRenderer->Render();
+	m_fpsTextRenderer->Render();
+
+	return true;
+}
+
+void App2::App2Main::OnPointerPressed(Windows::UI::Core::CoreWindow ^ sender, Windows::UI::Core::PointerEventArgs ^ args)
+{
+}
+
+void App2::App2Main::OnPointerReleased(Windows::UI::Core::CoreWindow ^ sender, Windows::UI::Core::PointerEventArgs ^ args)
+{
+}
+
+void App2::App2Main::OnPointerMoved(Windows::UI::Core::CoreWindow ^ sender, Windows::UI::Core::PointerEventArgs ^ args)
+{
+}
+
+// Notifies renderers that device resources need to be released.
+void App2Main::OnDeviceLost()
+{
+	m_sceneRenderer->ReleaseDeviceDependentResources();
+	m_fpsTextRenderer->ReleaseDeviceDependentResources();
+}
+
+// Notifies renderers that device resources may now be recreated.
+void App2Main::OnDeviceRestored()
+{
+	m_sceneRenderer->CreateDeviceDependentResources();
+	m_fpsTextRenderer->CreateDeviceDependentResources();
+	CreateWindowSizeDependentResources();
+}
---
+++ App2/App2/App2Main.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#include "Common\StepTimer.h"
+#include "Common\DeviceResources.h"
+#include "Content\Sample3DSceneRenderer.h"
+#include "Content\SampleFpsTextRenderer.h"
+
+// Renders Direct2D and 3D content on the screen.
+namespace App2
+{
+	class App2Main : public DX::IDeviceNotify
+	{
+	public:
+		App2Main(const std::shared_ptr<DX::DeviceResources>& deviceResources);
+		~App2Main();
+		void CreateWindowSizeDependentResources();
+		void Update();
+		bool Render();
+
+		void OnPointerPressed(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::PointerEventArgs^ args);
+		void OnPointerReleased(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::PointerEventArgs^ args);
+		void OnPointerMoved(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::PointerEventArgs^ args);
+
+		// IDeviceNotify
+		virtual void OnDeviceLost();
+		virtual void OnDeviceRestored();
+
+	private:
+		// Cached pointer to device resources.
+		std::shared_ptr<DX::DeviceResources> m_deviceResources;
+
+		// TODO: Replace with your own content renderers.
+		std::unique_ptr<Sample3DSceneRenderer> m_sceneRenderer;
+		std::unique_ptr<SampleFpsTextRenderer> m_fpsTextRenderer;
+
+		// Rendering loop timer.
+		DX::StepTimer m_timer;
+	};
+}
